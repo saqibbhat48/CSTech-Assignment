8d965e82127a2534ae12716f11b6e06deafee379
saqibbhat48 <bhatsaqib48@gmail.com>
2025-08-01 21:22:19 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const listRoutes = require('./routes/listRoutes');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser());
