e41bdec085d692060cd615ed3789659c8458be63
saqibbhat48 <bhatsaqib48@gmail.com>
2025-08-01 21:36:35 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,41 @@
-const app = require('./app');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv")
 dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const path = require("path")
+const app = express();
 
-mongoose.connect(process.env.MONGO_URI)
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoutes');
+const agentRoutes = require('./routes/agentRoutes');
+const listRoutes = require('./routes/listRoutes');
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/agents', agentRoutes);
+app.use('/api/lists', listRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
+
+  
+  
+  mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
     app.listen(process.env.PORT, () => console.log('Server on port', process.env.PORT));
   })
   .catch(err => console.error('MongoDB connection error:'));
+  
